70c57d0b9d46272072dcccb68369fcf9d6b8f491
Hristo Ivanov <103217093+hristo10000@users.noreply.github.com>
2022-07-12 14:42:18 +0300

    fixed an eslint issue and improved containsAll

--- 04_getting_started_js/containsAll.js
+++ 04_getting_started_js/containsAll.js
@@ -1,13 +1,14 @@
-/* eslint-disable linebreak-style */
 const containsAll = function(elements, arr) {
-    let flag = true;
-    elements.forEach((element) => {
-      if (!arr.includes(element)) {
-        flag = flase;
-      };
-    });
-    return flag;
-  };
-  
-  console.log(containsAll([1, 2, 3], [1, 2, 3, 4, 5]));
-  
+  const allFound = elements.every( (ai) => {
+    const helpArr = arr.filter((el) => el === ai);
+    if (helpArr.length === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+  return allFound ? true : false;
+};
+
+console.log(containsAll([1, 2], [3, 2, 1]));
+console.log(containsAll([1, 5], [3, 2, 1]));
