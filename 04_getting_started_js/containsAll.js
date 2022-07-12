df7229ceaf4061defee27153746e888d819aa6e3
Hristo Ivanov <103217093+hristo10000@users.noreply.github.com>
2022-07-12 14:54:15 +0300

    Update containsAll.js

--- 04_getting_started_js/containsAll.js
+++ 04_getting_started_js/containsAll.js
@@ -1,6 +1,6 @@
 const containsAll = function(elements, arr) {
-  const allFound = elements.every( (ai) => {
-    const helpArr = arr.filter((el) => el === ai);
+  const allFound = elements.every( (elementsEl) => {
+    const helpArr = arr.filter((arrEl) => arrEl === elementsEl);
     if (helpArr.length === 0) {
       return false;
     } else {
@@ -10,5 +10,5 @@ const containsAll = function(elements, arr) {
   return allFound ? true : false;
 };
 
-console.log(containsAll([1, 2], [3, 2, 1]));
-console.log(containsAll([1, 5], [3, 2, 1]));
+console.log(containsAll([1, 2], [1, 2, 3, 4, 5]));
+console.log(containsAll([1, 5], [1, 2, 3, 4]));
