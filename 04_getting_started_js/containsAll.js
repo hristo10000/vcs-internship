45ac55bc3dd400db47d80b869e07bd48d47425dd
Hristo Ivanov <103217093+hristo10000@users.noreply.github.com>
2022-07-14 10:45:57 +0300

    simplified containsAll

--- 04_getting_started_js/containsAll.js
+++ 04_getting_started_js/containsAll.js
@@ -1,14 +1,8 @@
 const containsAll = function(elements, arr) {
-  const allFound = elements.every( (elementsEl) => {
-    const helpArr = arr.filter((arrEl) => arrEl === elementsEl);
-    if (helpArr.length === 0) {
-      return false;
-    } else {
-      return true;
-    }
+  return elements.every((elementToCheck) =>{
+    return arr.some((arrElement) => arrElement===elementToCheck);
   });
-  return allFound ? true : false;
 };
 
-console.log(containsAll([1, 2], [1, 2, 3, 4, 5]));
-console.log(containsAll([1, 5], [1, 2, 3, 4]));
+console.log(containsAll([1, 2], [1, 2, 3, 4, 5])); // true
+console.log(containsAll([1, 5], [1, 2, 3, 4])); // false
