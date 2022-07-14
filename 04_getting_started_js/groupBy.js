aebe0990f11936ccd02f77e50a3cdddcb80cbd86
Hristo Ivanov <103217093+hristo10000@users.noreply.github.com>
2022-07-14 16:30:55 +0300

    groupBy now follows the given signature

--- 04_getting_started_js/groupBy.js
+++ 04_getting_started_js/groupBy.js
@@ -1,32 +1,32 @@
+const groupBy = function(groupingFunction, arr) {
+  return arr.reduce((result, item) => {
+    const key = groupingFunction(item);
+    if (!result[key]) {
+      result[key] = [];
+    }
+    result[key].push(item);
+    return result;
+  }, {});
+
+};
+
 const students = [{
-    'name': 'Daniel Taskoff',
-    'course': 'Frontend JavaScript',
-  }, {
-    'name': 'Elena Jeleva',
-    'course': 'Programming 101',
-  }, {
-    'name': 'Luboslava Dimitrova',
-    'course': 'Frontend JavaScript',
-  }, {
-    'name': 'Anton Antonov',
-    'course': 'Core Java',
-  }, {
-    'name': 'Nikola Dichev',
-    'course': 'Core Java',
-  }];
-  
-  const groupBy = function(property, objectArray) {
-    return objectArray.reduce((acc, obj) => {
-      const key = obj[property];
-      if (!acc[key]) {
-        acc[key] = [];
-      }
-      // Add object to list for given key's value
-      acc[key].push(obj);
-      return acc;
-    }, {});
-  };
-  
-  
-  console.log(groupBy('course', students));
-  
+  'name': 'Daniel Taskoff',
+  'course': 'Frontend JavaScript',
+}, {
+  'name': 'Elena Jeleva',
+  'course': 'Programming 101',
+}, {
+  'name': 'Luboslava Dimitrova',
+  'course': 'Frontend JavaScript',
+}, {
+  'name': 'Anton Antonov',
+  'course': 'Core Java',
+}, {
+  'name': 'Nikola Dichev',
+  'course': 'Core Java',
+}];
+
+console.log(groupBy(function(student) {
+  return student.course;
+}, students));
