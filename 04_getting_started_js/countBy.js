8b13354b4779b79009d27833032984ae15c19843
Hristo Ivanov <103217093+hristo10000@users.noreply.github.com>
2022-07-14 15:35:13 +0300

    countBy changed.
    now it's following the given signature

--- 04_getting_started_js/countBy.js
+++ 04_getting_started_js/countBy.js
@@ -1,31 +1,31 @@
+const countBy = function(countingFunction, arr) {
+  return arr.reduce((result, item) => {
+    const key = countingFunction(item);
+    if (!result[key]) {
+      result[key] = 0;
+    }
+    result[key] ++;
+    return result;
+  }, {});
+};
+
 const students = [{
-    'name': 'Daniel Taskoff',
-    'course': 'Frontend JavaScript',
-  }, {
-    'name': 'Elena Jeleva',
-    'course': 'Programming 101',
-  }, {
-    'name': 'Luboslava Dimitrova',
-    'course': 'Frontend JavaScript',
-  }, {
-    'name': 'Anton Antonov',
-    'course': 'Core Java',
-  }, {
-    'name': 'Nikola Dichev',
-    'course': 'Core Java',
-  }];
-  
-  const countBy = function(property, objectArray) {
-    return objectArray.reduce((acc, obj) => {
-      const key = obj[property];
-      if (!acc[key]) {
-        acc[key] = 0;
-      }
-      // Add object to list for given key's value
-      acc[key] ++;
-      return acc;
-    }, {});
-  };
-  
-  console.log(countBy('course', students));
-  
+  'name': 'Daniel Taskoff',
+  'course': 'Frontend JavaScript',
+}, {
+  'name': 'Elena Jeleva',
+  'course': 'Programming 101',
+}, {
+  'name': 'Luboslava Dimitrova',
+  'course': 'Frontend JavaScript',
+}, {
+  'name': 'Anton Antonov',
+  'course': 'Core Java',
+}, {
+  'name': 'Nikola Dichev',
+  'course': 'Core Java',
+}];
+
+console.log(countBy(function(student) {
+  return student.course;
+}, students));
